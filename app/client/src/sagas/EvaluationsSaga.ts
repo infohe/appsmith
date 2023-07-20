21924ed5d9b9d511557ea671a2a6a58143ee3335
Favour Ohans <fohanekwu@gmail.com>
2023-07-20 08:39:07 +0100

    Fix dependency update issues

--- app/client/src/sagas/EvaluationsSaga.ts
+++ app/client/src/sagas/EvaluationsSaga.ts
@@ -218,6 +218,7 @@ export function* updateDataTreeHandler(
       jsData,
     );
   }
+
   yield put(setDependencyMap(dependencies));
   if (postEvalActionsToDispatch && postEvalActionsToDispatch.length) {
     yield call(postEvalActionDispatcher, postEvalActionsToDispatch);
