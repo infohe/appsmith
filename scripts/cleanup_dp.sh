abebcb98006ac2b0f11d5e2b914af53e1dc6e84b
Goutham Pratapa <goutham@appsmith.com>
2023-07-23 21:34:14 +0530

    update: migrate dp from sts to deployments

--- scripts/cleanup_dp.sh
+++ scripts/cleanup_dp.sh
@@ -14,6 +14,7 @@ output = json
 region=ap-south-1
 source_profile = default" > ~/.aws/config
 
+
 sts_output=$(aws sts assume-role --role-arn env.AWS_ROLE_ARN --role-session-name ekscisession)
 export AWS_ACCESS_KEY_ID=$(echo $sts_output | jq -r '.Credentials''.AccessKeyId');\
 export AWS_SECRET_ACCESS_KEY=$(echo $sts_output | jq -r '.Credentials''.SecretAccessKey');\
@@ -28,7 +29,7 @@ echo "Getting the pods"
 kubectl get pods
 
 ### Get list of helm charts
-deployed_charts="$(helm ls -A --filter 'ce[0-9]+' --output json | jq -r '.[].namespace')"
+deployed_charts="$(helm ls -A --filter 'ee[0-9]+' --output json | jq -r '.[].namespace')"
 
 for i in $deployed_charts
   do 
@@ -40,5 +41,7 @@ for i in $deployed_charts
       helm uninstall $i -n $i
       kubectl delete ns $i || true
       mongosh "mongodb+srv://$DB_USERNAME:$DB_PASSWORD@$DB_URL/$i?retryWrites=true&minPoolSize=1&maxPoolSize=10&maxIdleTimeMS=900000&authSource=admin" --eval 'db.dropDatabase()'
+      ACCESS_POINT_ID=$(aws efs describe-access-points --file-system-id "$DP_EFS_ID" | jq -r '.AccessPoints[] | select(.Name=="$pr") | .AccessPointId')
+      aws efs delete-access-point --access-point-id $ACCESS_POINT_ID
     fi
   done
