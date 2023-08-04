1c8d3c9b25468bdf9cd74efb8e6ce9d917ac4f96
Ashiq KS <ashiqgiga07@gmail.com>
2023-08-04 21:45:02 +0530

    Overflow & height style

--- app/client/src/widgets/RJSFBuilderWidget/component/index.tsx
+++ app/client/src/widgets/RJSFBuilderWidget/component/index.tsx
@@ -2,14 +2,20 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { FormBuilder } from "@ginkgo-bioworks/react-json-schema-form-builder";
 import type { ComponentProps } from "widgets/BaseComponent";
+import type { CSSProperties } from "react";
 
 function RJSFBuilderComponent(props: RJSFBuilderComponentProps) {
   const { onChange, schema: initSchema, uischema: initUischema } = props;
+  const style: CSSProperties = {
+    overflow: "auto !important",
+    height: "auto !important",
+  };
 
   return (
     <FormBuilder
       onChange={onChange}
       schema={initSchema}
+      style={style}
       uischema={initUischema}
     />
   );
