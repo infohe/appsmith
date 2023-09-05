599b4f6e6e4c9aa46433571151fa7b064aa6c22d
Ashiq KS <ashiqgiga07@gmail.com>
2023-09-05 14:44:57 +0530

    Group rjsf

--- app/client/src/widgets/RJSFBuilderWidget/index.ts
+++ app/client/src/widgets/RJSFBuilderWidget/index.ts
@@ -1,10 +1,12 @@
 import Widget from "./widget";
 import IconSVG from "./icon.svg";
+import { WIDGET_TAGS } from "constants/WidgetConstants";
 
 export const CONFIG = {
   type: Widget.getWidgetType(),
   name: "RJSFBuilder", // The display name which will be made in uppercase and show in the widgets panel ( can have spaces )
   iconSVG: IconSVG,
+  tags: [WIDGET_TAGS.EXTERNAL],
   needsMeta: true, // Defines if this widget adds any meta properties
   isCanvas: false, // Defines if this widget has a canvas within in which we can drop other widgets
   features: {
